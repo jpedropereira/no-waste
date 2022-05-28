cae3aec31e076a514c779e26c11c2139ad639991
Joao Pereira <jpedropereira89@gmail.com>
2022-05-28 15:38:39 +0200

    Registered get_recipes app in settings.py and created get_recipes view and urls

--- get_recipes/views.py
+++ get_recipes/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def get_recipe(request):
+    return render(request, "get_recipes/get_recipes.html")
--- no_waste/settings.py
+++ no_waste/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'get_recipes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
