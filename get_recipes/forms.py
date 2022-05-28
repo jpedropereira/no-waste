525ffdcfcee15e25035e490b3f407fb99f47e602
Joao Pereira <jpedropereira89@gmail.com>
2022-05-28 18:42:35 +0200

    Added GetRecipesForm class

--- get_recipes/forms.py
+++ get_recipes/forms.py
@@ -1,2 +1,17 @@
 from django import forms
 
+class GetRecipesForm(forms.Form):
+    ingredients_to_incluide = forms.CharField(
+        max_length=200,
+        label="What ingredients do you want to incluide? Separate them with a comma and without spaces in between",
+        required=True
+        )
+    ingredients_to_exclude = forms.CharField(
+        max_length=200, 
+        required=False
+        )
+    recepies_number = forms.IntegerField(
+        min_value=0, 
+        max_value=5,
+        required=True
+        )
