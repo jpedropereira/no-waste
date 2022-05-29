2d550a356b4e3d8af983d5de93d4deffaadc53c7
Joao Pereira <jpedropereira89@gmail.com>
2022-05-29 22:44:23 +0200

    added ability to get recipes from spoontacular

--- get_recipes/views.py
+++ get_recipes/views.py
@@ -1,9 +1,19 @@
+import os
+import requests
 from django.shortcuts import render
 from django.views.generic.edit import FormView
+from dotenv import load_dotenv
 
 from .forms import GetRecipesForm
 
-# Create your views here.
+load_dotenv()
+
+API_KEY = os.getenv("SPOONTACULAR_API_KEY")
+API_ENDPOINT = "https://api.spoonacular.com/recipes/complexSearch"
+
+
+#Create your views here.
+
 class GetRecipesView(FormView):
     form_class = GetRecipesForm
     template_name = "get_recipes/get_recipes.html"
@@ -14,15 +24,37 @@ class GetRecipesView(FormView):
         return super().form_valid(form)
 
 
+def get_spoontacular_data(include, exclude, number):
+    recipe_config = {
+        "apiKey": API_KEY,
+        "includeIngredients": include,
+        "excludeIngredients": exclude,
+        "instructionsRequired": True,
+        "addRecipeNutrition": True,
+        "sort": "min-missing-ingredients",
+        "number": number,
+        }
+
+    response = requests.get(url=API_ENDPOINT, params=recipe_config)
+    response.raise_for_status()
+    recipes = response.json()
+
+    print(recipes)
+
+    return recipes
+
+
 def search_recipes(request):
     query_dict = request.GET
     ingredients_to_include = query_dict["ingredients_to_include"]
     ingredients_to_exclude = query_dict["ingredients_to_exclude"]
     recipes_number = query_dict["recipes_number"]
-    print(ingredients_to_include)
+
+    recipes = get_spoontacular_data(ingredients_to_include, ingredients_to_exclude, recipes_number)
+    print(recipes)
+
     context = {
-        "ingredients_to_include": ingredients_to_include,
-        "ingredients_to_exclude": ingredients_to_exclude,
-        "recipes_number": recipes_number,
+        "recipes": recipes
     }
     return render(request, "get_recipes/recipes_list.html", context=context)
+
