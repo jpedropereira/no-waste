14d8bf06175e627c7e8e10080b47b101df1ac1ee
Joao Pereira <84179710+jpedropereira@users.noreply.github.com>
2022-06-08 20:19:06 +0200

    Created Recipe class in models.py

--- no_waste/recipes/models.py
+++ no_waste/recipes/models.py
@@ -1,6 +1,17 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
 
 class Ingredient(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    picture_url = models.URLField()
+    ingredients = models.ManyToManyField(Ingredient)
+    carbs = models.FloatField()
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    calories = models.FloatField()
+    sourceurl = models.URLField()
