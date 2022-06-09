4a0abacc081cc65e0cd5343fb59262ff731219ca
Joao Pereira <84179710+jpedropereira@users.noreply.github.com>
2022-06-10 01:33:34 +0200

    Added SearchQuery class in models.py

--- no_waste/recipes/models.py
+++ no_waste/recipes/models.py
@@ -5,12 +5,20 @@ from django.db import models
 # Create your models here.
 
 
+
+
 class Recipe(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     picture_url = models.URLField()
-    ingredients = models.CharField()
+    ingredients = models.CharField(max_length=500)
     carbs = models.FloatField()
     proteins = models.FloatField()
     fats = models.FloatField()
     calories = models.FloatField()
-    sourceurl = models.URLField()
+    sourceurl = models.URLField(unique=True)
+
+class SearchQuery(models.Model):
+    search_query = models.CharField(max_length=500)
+    recipes = models.ManyToManyField(Recipe)
+
+    
