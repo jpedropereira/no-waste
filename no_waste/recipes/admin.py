ef654b2775567a20952bd69f7152f34625e66ec2
Joao Pereira <84179710+jpedropereira@users.noreply.github.com>
2022-06-10 01:08:38 +0200

    Removed ingredients module and admin config

--- no_waste/recipes/admin.py
+++ no_waste/recipes/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
-from recipes.models import Recipe, Ingredient
 
 # Register your models here.
 
-class IngredientAdmin(admin.ModelAdmin):
-    list_display = ("name",)
 
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("title",)
-
-admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(Recipe, RecipeAdmin)
 
--- no_waste/recipes/models.py
+++ no_waste/recipes/models.py
@@ -1,15 +1,14 @@
+from cgi import test
 from operator import mod
 from django.db import models
 
 # Create your models here.
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
 
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     picture_url = models.URLField()
-    ingredients = models.ManyToManyField(Ingredient)
+    ingredients = models.CharField()
     carbs = models.FloatField()
     proteins = models.FloatField()
     fats = models.FloatField()
