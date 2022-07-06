c5d06fbbfa984441bbb5b6cc741f86cbc2916d3d
Joao Pereira <84179710+jpedropereira@users.noreply.github.com>
2022-07-06 22:38:58 +0200

    Removed admin

--- no_waste/recipes/admin.py
+++ no_waste/recipes/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
-
-from recipes.models import Recipe, SearchQuery
-
 # Register your models here.
-
-admin.site.register(Recipe)
-admin.site.register(SearchQuery)
-
-
